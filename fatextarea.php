38314bba2423df1d8410052215aa0cefad503ec4
yonadi.com <yonadi.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-11 17:59:47 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fatextarea/trunk@408426 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fatextarea.php
+++ fatextarea.php
@@ -3,14 +3,14 @@
 Plugin Name: fatextarea
 Plugin URI: http://www.yonadi.com
 Description: change textarea from rtl to ltr .
-Version: 1.0
+Version: 1.1
 Author: abdolmajed shah bakhsh ( ijbari )
 Author URI: http://www.yonadi.com/
 */
 function addHeaderCode_fatextarea() {
 		echo "\n".'<!-- Start fatextarea by abdolmajed -->'."\n";
 		echo '<style type="text/css">' . "\n";
-		echo ' textarea {direction:ltr;}' . "\n";
+		echo ' textarea {direction:ltr;} #replycontent {direction:rtl!important;' . "\n";
 		echo '</style>'."\n";
 		echo '<!-- Stop fatextarea by http://yonadi.com -->'."\n";
 }
